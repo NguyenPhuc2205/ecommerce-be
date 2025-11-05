fb081a3e97af6078a95183cc74025625be045882
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-11-05 22:02:42 +0700

    refactor(middleware): modify import path

--- src/common/middlewares/trace-id.middleware.ts
+++ src/common/middlewares/trace-id.middleware.ts
@@ -1,13 +1,13 @@
+import { CUSTOM_HEADERS, REQUEST_CONTEXTS } from '@/common/constants'
 import { Injectable, NestMiddleware } from '@nestjs/common'
 import { NextFunction, Request, Response } from 'express'
-import { CUSTOM_HEADER_KEY, TRACE_ID_KEY } from '@/common/constants'
 import { v4 as uuidv4 } from 'uuid'
 
 @Injectable()
 export class TraceMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const headerKey = CUSTOM_HEADER_KEY.TRACE_ID
-    const traceIdKey = TRACE_ID_KEY
+    const headerKey = CUSTOM_HEADERS.TRACE_ID
+    const traceIdKey = REQUEST_CONTEXTS.TRACE_ID
 
     // Extract from headers or generate a new trace id
     const traceId = (req.headers[headerKey] as string | undefined) ?? uuidv4()
