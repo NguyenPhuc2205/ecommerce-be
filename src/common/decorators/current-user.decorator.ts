6a66208979b0560c185cee4240262c7e087d9233
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-11-09 16:01:07 +0700

    refactor(common): use new constants in decorators

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,12 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 import { Request } from 'express'
-import { JwtPayloadFieldType, REQUEST_CURRENT_USER_KEY } from '@/common/constants'
 import { IJwtPayload } from '@/common/interfaces'
+import { JwtPayloadFieldType, REQUEST_CONTEXTS } from '@/common/constants'
 
 export const CurrentUser = createParamDecorator((field: JwtPayloadFieldType | undefined, ctx: ExecutionContext) => {
   // Get request object from http context
   const request: Request = ctx.switchToHttp().getRequest<Request>()
-  const user: IJwtPayload | undefined = request[REQUEST_CURRENT_USER_KEY]
+  const user: IJwtPayload | undefined = request[REQUEST_CONTEXTS.CURRENT_USER]
 
   // If user is not found, return undefined
   if (!user) return undefined
