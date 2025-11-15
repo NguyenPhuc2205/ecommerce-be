3d57d33c2c920902f33cffac6e182115aa487646
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-11-15 21:48:15 +0700

    fix(prisma): modify transaction service

--- src/shared/database/transaction.service.ts
+++ src/shared/database/transaction.service.ts
@@ -1,18 +1,18 @@
-import { TransactionOptions } from '@/common/interfaces'
+import { PRISMA_DEFAULTS } from '@/common/constants'
+import { ITransactionOptions } from '@/common/interfaces'
 import { PrismaTransactionClient } from '@/common/types'
 import { PrismaService } from '@/shared/database/prisma.service'
 import { Injectable } from '@nestjs/common'
-import { Prisma } from '@prisma/client'
 
 @Injectable()
 export class TransactionService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async run<T>(callback: (tx: PrismaTransactionClient) => Promise<T>, options?: TransactionOptions): Promise<T> {
-    const mergedOptions: TransactionOptions = {
-      maxWait: 5000,
-      timeout: 30000,
-      isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted,
+  async run<T>(callback: (tx: PrismaTransactionClient) => Promise<T>, options?: ITransactionOptions): Promise<T> {
+    const mergedOptions: ITransactionOptions = {
+      maxWait: PRISMA_DEFAULTS.MAX_WAIT,
+      timeout: PRISMA_DEFAULTS.TIMEOUT,
+      isolationLevel: PRISMA_DEFAULTS.ISOLATION_LEVEL,
       ...options,
     }
 
